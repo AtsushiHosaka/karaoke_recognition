f40702c402c90e482b412b1a33b51542d0ebd74f
AtsushiHosaka <hosaka2917@gmail.com>
2023-05-07 11:46:45 +0900

    Initial Commit

---
+++ Karaoke_recognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Karaoke_recognition
+//
+//  Created by 保坂篤志 on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
